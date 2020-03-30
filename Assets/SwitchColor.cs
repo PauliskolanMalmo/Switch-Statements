1119d1a728af48ae02b223747202a8c9829c590b
Paul B <paul.belfrage@gmail.com>
2020-03-30 13:31:57 +0200

    Challenge Review Master Switch Statements P2
    Challenge Review Master Switch Statements P2

---
+++ Assets/SwitchColor.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchColor : MonoBehaviour
+{
+    public GameObject cube;
+    private int nextColor;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            nextColor = 0;
+            //cube.GetComponent<Renderer>().material.color = Color.blue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            nextColor = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            nextColor = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            nextColor = 3;
+        }
+
+        switch (nextColor)
+        {
+            case 0:
+                cube.GetComponent<Renderer>().material.color = Color.blue;
+                break;
+            case 1:
+                cube.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case 2:
+                cube.GetComponent<Renderer>().material.color = Color.green;
+                break;
+            case 3:
+                cube.GetComponent<Renderer>().material.color = Color.black;
+                break;
+                /*default:
+                    print("Invalid Selection");
+                    break;*/
+
+
+        }
+
+
+    }
+}
+
